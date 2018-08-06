30f75136f1bb054f571e62ae9cfbb623349abf0c
karol <karolsiegied@gmail.com>
2018-08-06 22:46:17 +0200

    New parse command parameters

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <unistd.h>
 
 int main(int argc, char *argv[])
 {
+    int c;
+    while ((c = getopt(argc, argv, "lh")) != -1) {
+        switch (c) {
+        case 'h':
+            qDebug() << "Usage: \n" << "h - help \n" \
+                     << "l - debug level \n";
+            break;
+        case 'l':
+            qDebug() << "Debug level";
+            break;
+        }
+    }
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
