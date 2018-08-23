7f7a7d0d68e5fbc369b69e100e0f5bf2b70530f1
karol <karolsiegied@gmail.com>
2018-08-23 23:57:33 +0200

    Init LedIndicator class

---
+++ src/ledindicator.h
@@ -0,0 +1,11 @@
+#ifndef LEDINDICATOR_H
+#define LEDINDICATOR_H
+#include <QAbstractButton>
+
+class LedIndicator : public QAbstractButton
+{
+public:
+    LedIndicator();
+};
+
+#endif // LEDINDICATOR_H
