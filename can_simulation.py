8108c253ac75adebc13fbfb957c4ac61e36cd292
karol <karolsiegied@gmail.com>
2018-09-05 17:30:41 +0200

    Added can simulation python script

---
+++ can_simulation.py
@@ -0,0 +1,16 @@
+from sys import stdout
+import time
+lines = []
+
+with open('../log/gokart_log.txt') as f:
+    for line in f.readlines():
+        lines.append(line.replace('\n', ''))
+
+i = 0
+length = len(lines)
+while True:
+    print lines[i%length]
+    stdout.flush()
+    time.sleep(0.04)
+    i += 1
+    
